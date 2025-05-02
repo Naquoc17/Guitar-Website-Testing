f34543822f4b60e95b7095e2bcd9b063e251d434
naquoc17.konto@gmail.com <naquoc17.konto@gmail.com>
2025-05-02 13:06:55 +0200

    up

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
-import { render } from 'preact'
-import './index.css'
-import { App } from './app.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { App } from "./app.jsx";
 
-render(<App />, document.getElementById('app'))
+const root = createRoot(document.getElementById("app"));
+root.render(<App />);
