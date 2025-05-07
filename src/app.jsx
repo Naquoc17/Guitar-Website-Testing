413dbb91f6b83bc4ed75887edc9f9fd72988eb5b
naquoc17.konto@gmail.com <naquoc17.konto@gmail.com>
2025-05-07 13:14:26 +0200

    up

--- src/app.jsx
+++ src/app.jsx
@@ -8,18 +8,32 @@ export function App() {
    const [data, setData] = useState(db);
    const [cart, setCart] = useState([]);
 
-   console.log(cart);
+   function addToCart(item) {
+      const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+      if (itemExists >= 0) {
+         const updatedCart = [...cart];
+         updatedCart[itemExists].quantity += 1;
+         setCart(updatedCart);
+      } else {
+         item.quantity = 1;
+         setCart([...cart, item]);
+      }
+   }
 
    return (
       <>
-         <Header />
+         <Header cart={cart} />
 
          <main className="container-xl mt-5">
             <h2 className="text-center">Nuestra Colección</h2>
 
             <div className="row mt-5">
                {data.map((guitar) => (
-                  <Guitar key={guitar.id} guitar={guitar} setCart={setCart} />
+                  <Guitar
+                     key={guitar.id}
+                     guitar={guitar}
+                     addToCart={addToCart}
+                  />
                ))}
             </div>
          </main>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-export default function Header() {
+export default function Header({ cart }) {
    return (
       <>
          <header className="py-5 header">
@@ -34,40 +34,42 @@ export default function Header() {
                                  </tr>
                               </thead>
                               <tbody>
-                                 <tr>
-                                    <td>
-                                       <img
-                                          className="img-fluid"
-                                          src="./public/img/guitarra_02.jpg"
-                                          alt="imagen guitarra"
-                                       />
-                                    </td>
-                                    <td>SRV</td>
-                                    <td className="fw-bold">$299</td>
-                                    <td className="flex align-items-start gap-4">
-                                       <button
-                                          type="button"
-                                          className="btn btn-dark"
-                                       >
-                                          -
-                                       </button>
-                                       1
-                                       <button
-                                          type="button"
-                                          className="btn btn-dark"
-                                       >
-                                          +
-                                       </button>
-                                    </td>
-                                    <td>
-                                       <button
-                                          className="btn btn-danger"
-                                          type="button"
-                                       >
-                                          X
-                                       </button>
-                                    </td>
-                                 </tr>
+                                 {cart.map((guitar) => (
+                                    <tr>
+                                       <td>
+                                          <img
+                                             className="img-fluid"
+                                             src="./public/img/guitarra_02.jpg"
+                                             alt="imagen guitarra"
+                                          />
+                                       </td>
+                                       <td>SRV</td>
+                                       <td className="fw-bold">$299</td>
+                                       <td className="flex align-items-start gap-4">
+                                          <button
+                                             type="button"
+                                             className="btn btn-dark"
+                                          >
+                                             -
+                                          </button>
+                                          1
+                                          <button
+                                             type="button"
+                                             className="btn btn-dark"
+                                          >
+                                             +
+                                          </button>
+                                       </td>
+                                       <td>
+                                          <button
+                                             className="btn btn-danger"
+                                             type="button"
+                                          >
+                                             X
+                                          </button>
+                                       </td>
+                                    </tr>
+                                 ))}
                               </tbody>
                            </table>
 
